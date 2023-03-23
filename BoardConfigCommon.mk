9ca301858e110c6a133da8691f7512badbbd0971
darkcamper242 <sided9898@gmail.com>
2023-03-23 21:15:51 +0300

    inital commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from universal5420-common
+include device/samsung/universal5420-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/ha3g-common
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# Audio
+BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
+
+# Camera: portrait orientation
+BOARD_CAMERA_FRONT_ROTATION := 270
+BOARD_CAMERA_BACK_ROTATION := 90
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+BOARD_NEEDS_MEMORYHEAPION := true
+BOARD_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
+
+# Mixer
+BOARD_USE_BGRA_8888 := true
+
+# HDMI
+BOARD_USES_GSC_VIDEO := true
+
+# Battery
+RED_LED_PATH := "/sys/class/leds/led_r/brightness" 
+GREEN_LED_PATH := "/sys/class/leds/led_g/brightness" 
+BLUE_LED_PATH := "/sys/class/leds/led_b/brightness" 
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+
+# Audio blobs
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+
+# Shims
+TARGET_LD_SHIM_LIBS += \
+    /vendor/bin/gpsd|/vendor/lib/libshim_dmitry_gps.so \
+   /system/vendor/lib/libsec-ril.so|libshim_atomic.so \
+   /system/vendor/lib/libsec-ril.so|/vendor/lib/libcutils_shim.so
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+BOARD_GLOBAL_CFLAGS += -DSEC_PRODUCT_FEATURE_RIL_CALL_DUALMODE_CDMAGSM
+
+# Inherit from the proprietary version
+-include vendor/samsung/ha3g-common/BoardConfigVendor.mk
---
+++ device-common.mk
@@ -0,0 +1,113 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+DEVICE_PATH := device/samsung/ha3g-common
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    device/samsung/ha3g-common/overlay/lineage-sdk \
+    device/samsung/ha3g-common/overlay/hardware/samsung/AdvancedDisplay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+# AdvancedDisplay (MDNIE)
+PRODUCT_PACKAGES += \
+    AdvancedDisplay 
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/bt_vendor.conf
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/e2fsck:$(TARGET_COPY_OUT_SYSTEM)/bin/e2fsck
+
+
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
+    $(DEVICE_PATH)/configs/audio/ysound.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/ysound.xml \
+    $(DEVICE_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(DEVICE_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/mixer_paths.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Key-layout
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/sec_e-pen.idc \
+    $(DEVICE_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/Synaptics_HID_TouchPad.idc \
+    $(DEVICE_PATH)/configs/idc/Synaptics_RMI4_TouchPad_Sensor.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc \
+    $(DEVICE_PATH)/configs/keylayout/Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Button_Jack.kl \
+    $(DEVICE_PATH)/configs/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl \
+    $(DEVICE_PATH)/configs/keylayout/sec_e-pen.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_e-pen.kl \
+    $(DEVICE_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_touchscreen.kl
+
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# Shims
+PRODUCT_PACKAGES += \
+libshim_dmitry_gps \
+    libshim_atomic \
+    libcutils_shim \
+    libshim_binder
+    
+ # Macloader
+ PRODUCT_PACKAGES += \
+     macloader
+ 
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze 
+     
+# GPS
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/gps/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gps.xml \
+    $(DEVICE_PATH)/configs/gps/gps.cer:$(TARGET_COPY_OUT_VENDOR)/bin/gps.cer     
+        
+
+ # Camera
+ PRODUCT_PACKAGES += \
+     libhwjpeg \
+     libsecnativefeature 
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.universal5420
+     
+# libstlport
+ PRODUCT_PACKAGES += \
+     libstlport     
+
+# Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
+
+# Inherit from universal5420-common
+$(call inherit-product, device/samsung/universal5420-common/device-common.mk)
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/ha3g-common/ha3g-common-vendor.mk)
